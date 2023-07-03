b0b95a89ccc98cdc36968ef21d72034e225adc42
cllanosucb <cllanos@ucb.edu.bo>
2023-07-02 22:56:56 -0400

    Modal Usuarios personalizado terminado

--- src/app/protected/pages/modal-components/modal-usuario/modal-usuario.component.ts
+++ src/app/protected/pages/modal-components/modal-usuario/modal-usuario.component.ts
@@ -4,6 +4,7 @@ import { Roles } from 'src/app/protected/interfaces/roles';
 
 import { Usuarios } from 'src/app/protected/interfaces/usuarios';
 import { RolesService } from 'src/app/protected/services/roles.service';
+import { UsuariosService } from 'src/app/protected/services/usuarios.service';
 
 @Component({
     selector: 'app-modal-usuario',
@@ -13,14 +14,21 @@ import { RolesService } from 'src/app/protected/services/roles.service';
 export class ModalUsuarioComponent {
     @Input() titleButton!: String;
     @Input() usuario!: Usuarios;
-    @Input() tipoAccion!: number; //1=agregar, 0 = ver, 2=editar
+    @Input() tipoAccion!: number; //1=agregar, 0 = ver, 2=editar, 3 = eliminar, 4 = habilitar
 
     @Output() onCloseModal: EventEmitter<boolean> = new EventEmitter();
+    @Output() onSaveDataModal: EventEmitter<boolean> = new EventEmitter();
 
     roles: Roles[] = [];
     rol!: Roles;
     miFormulario: FormGroup = this.fb.group({
-        nombre: ['', [Validators.required, Validators.minLength(3)/* , Validators.maxLength(10) */]],
+        nombre: [
+            '',
+            [
+                Validators.required,
+                Validators.minLength(3) /* , Validators.maxLength(10) */,
+            ],
+        ],
         ap: ['', [Validators.required]],
         am: ['', [Validators.required]],
         ci: ['', [Validators.required]],
@@ -30,10 +38,14 @@ export class ModalUsuarioComponent {
         rol: ['', [Validators.required]],
     });
 
-    constructor(private rolesService: RolesService, private fb: FormBuilder) {}
+    constructor(
+        private rolesService: RolesService,
+        private fb: FormBuilder,
+        private usuariosService: UsuariosService
+    ) {}
 
     ngOnInit() {
-        // console.log(this.usuario);
+        console.log('modal usuario', this.usuario);
         this.getRoles();
     }
 
@@ -46,20 +58,27 @@ export class ModalUsuarioComponent {
     }
 
     cargarDatos() {
-        if(this.tipoAccion == 0 || this.tipoAccion == 2) {
+        if (this.tipoAccion == 0) this.miFormulario.disable();
+        if (this.tipoAccion != 1) {
             this.miFormulario.controls['nombre'].setValue(this.usuario.nombre);
             this.miFormulario.controls['ap'].setValue(this.usuario.ap);
             this.miFormulario.controls['am'].setValue(this.usuario.am);
             this.miFormulario.controls['ci'].setValue(this.usuario.ci);
-            this.miFormulario.controls['fecha_nac'].setValue(this.usuario.fecha_nac);
-            this.miFormulario.controls['usuario'].setValue(this.usuario.usuario);
-            this.miFormulario.controls['contrasena'].setValue(this.usuario.contrasena);
+            this.miFormulario.controls['fecha_nac'].setValue(
+                new Date(this.usuario.fecha_nac + '')
+            );
+            this.miFormulario.controls['usuario'].setValue(
+                this.usuario.usuario
+            );
+            this.miFormulario.controls['contrasena'].setValue(
+                this.usuario.contrasena
+            );
             this.miFormulario.controls['rol'].setValue(this.buscarRol());
         }
     }
 
     buscarRol() {
-        return this.roles.find(rol => rol.id_rol == this.usuario.id_rol);        
+        return this.roles.find((rol) => rol.id_rol == this.usuario.id_rol);
     }
 
     campoEsValido(campo: string) {
@@ -94,11 +113,78 @@ export class ModalUsuarioComponent {
     }
 
     guardar() {
+        console.log('miFormulario', this.miFormulario.value);
         if (this.miFormulario.invalid) {
             this.miFormulario.markAllAsTouched();
             return;
         }
-        console.log(this.miFormulario.value);
+        if (this.tipoAccion == 1) {
+            this.agregarUsuario();
+        }
+        if (this.tipoAccion == 2) {
+            this.editarUsuario();
+        }
+        if (this.tipoAccion == 3 || this.tipoAccion == 4) {
+            this.accionUsuario();
+        }
         this.miFormulario.reset();
     }
+
+    agregarUsuario() {
+        this.usuario.nombre = this.miFormulario.value.nombre;
+        this.usuario.ap = this.miFormulario.value.ap;
+        this.usuario.am = this.miFormulario.value.am;
+        this.usuario.ci = this.miFormulario.value.ci;
+        this.usuario.fecha_nac = this.miFormulario.value.fecha_nac;
+        this.usuario.usuario = this.miFormulario.value.usuario;
+        this.usuario.contrasena = this.miFormulario.value.contrasena;
+        this.usuario.estado = 1;
+        this.usuario.id_rol = this.miFormulario.value.rol.id_rol;
+        console.log('usuario', this.usuario);
+
+        this.usuariosService.agregarUsuario(this.usuario).subscribe((resp) => {
+            this.onSaveDataModal.emit(false);
+        });
+    }
+
+    editarUsuario() {
+        this.usuario.nombre = this.miFormulario.value.nombre;
+        this.usuario.ap = this.miFormulario.value.ap;
+        this.usuario.am = this.miFormulario.value.am;
+        this.usuario.ci = this.miFormulario.value.ci;
+        this.usuario.fecha_nac = this.miFormulario.value.fecha_nac;
+        this.usuario.usuario = this.miFormulario.value.usuario;
+        this.usuario.contrasena = this.miFormulario.value.contrasena;
+        this.usuario.estado = 1;
+        this.usuario.id_rol = this.miFormulario.value.rol.id_rol;
+        console.log('usuario', this.usuario);
+
+        this.usuariosService
+            .editarUsuario(this.usuario, this.usuario.id_usuario)
+            .subscribe((resp) => {
+                this.onSaveDataModal.emit(false);
+            });
+    }
+
+    accionUsuario() {
+        console.log('usuario', this.usuario);
+        if(this.tipoAccion == 3) {
+            // Eliminación de usuario
+            this.usuariosService.eliminarUsuario(this.usuario.id_usuario)
+                .subscribe((data) => {
+                    this.onSaveDataModal.emit(false);
+                });
+        }
+
+        if (this.tipoAccion == 4) {
+            // Habilitación de usuario
+            this.usuariosService
+                .habilitarUsuario(this.usuario.id_usuario)
+                .subscribe((data) => {
+                    this.onSaveDataModal.emit(false);
+                });
+        }
+            
+    }
+
 }
--- src/app/protected/services/usuarios.service.ts
+++ src/app/protected/services/usuarios.service.ts
@@ -11,16 +11,22 @@ export class UsuariosService {
   getUsuarios(){
     return this.http.get<Usuarios[]>('http://localhost:3000/api/usuarios')
   }
+  editarUsuario(Usuario:Usuarios, id_usuario: any){
+    return this.http.put<Usuarios[]>(
+        `http://localhost:3000/api/updateUsuario/${id_usuario}`,
+        Usuario
+    );
+  }
   agregarUsuario(Usuario:Usuarios){
     return this.http.post<Usuarios[]>('http://localhost:3000/api/createUsuario',Usuario);
   }
   verUsuario(id_usuario:number){
     return this.http.get<any>('http://localhost:3000/api/verUsuario/'+id_usuario);
   }
-  eliminarUsuario(id_usuario:number){
+  eliminarUsuario(id_usuario:any){
     return this.http.delete<any>('http://localhost:3000/api/deleteUsuario/'+id_usuario);
   }
-  habilitarUsuario(id_usuario:number){
+  habilitarUsuario(id_usuario:any){
     return this.http.get<any>('http://localhost:3000/api/habilitarUsuario/'+id_usuario);
   }
 }
